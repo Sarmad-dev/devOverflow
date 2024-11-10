b2de8dd25a21227fd4692798461abb4e75b0ea77
Sarmad Khan <sarmadkhan2694@gmail.com>
2024-11-11 00:14:28 +0500

    Implemented Right Sidebar

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,8 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "sign-up",
+  QUESTION: (_id: string) => `/question/${_id}`,
+  TAGS: (_id: string) => `/tags/${_id}`,
 };
 
 export default ROUTES;
