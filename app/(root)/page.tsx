9f5c8476d3c6f9fa242658d1e044a1dc5901e21a
Sarmad Khan <sarmadkhan2694@gmail.com>
2024-11-11 17:00:30 +0500

    Implement LocalSearch Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,38 @@
-const Home = async () => {
-  return <></>;
+import Link from "next/link";
+
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      HomeFIlter
+      <div className="mt-10 flex w-full flex-col gap-6"></div>
+    </>
+  );
 };
 
 export default Home;
--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,7 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "sign-up",
+  ASK_QUESTION: "/ask-question",
   QUESTION: (_id: string) => `/question/${_id}`,
   TAGS: (_id: string) => `/tags/${_id}`,
 };
